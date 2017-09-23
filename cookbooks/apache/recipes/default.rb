fc9f0de467af95da5c9add3c5ca7403c9a87d142
Philipp <philipp.c.zinser@gmail.com>
2017-09-23 04:44:09 +0000

    added postgresql cookbook

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -5,9 +5,9 @@
 # Copyright:: 2017, The Authors, All Rights Reserved.
 #install apache package
 
-if node[platform_family] == "rhel"
+if node['platform_family'] == "rhel"
 	package = "httpd"
-elsif node[platform_family] == "debian"
+elsif node['platform_family'] == "debian"
 	package = "apache2"
 end
 
